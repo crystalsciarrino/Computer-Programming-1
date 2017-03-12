31498376fb3179534bcdd870786dd921a935abf3
crystalsciarrino <crystal.sciarrino@gmail.com>
2017-03-12 09:27:42 -0700

    Create Interface_Celebrator.java

---
+++ Milestone assignment/Interface_Celebrator.java
@@ -0,0 +1,24 @@
+package milestone_assignment
+
+public interface Celebrator {
+	public void celebrate();
+		
+		public static void main (String args[]){
+		 Celebrator c = new Celebrator(); 
+	
+		 public String Dance(){
+		 return "Dances with pride";
+		}
+		 public String Fist(){
+			 return "Throws his fist in the air";
+		 }
+		 public String Point(){
+			 return "Points up to the lord with a hand on his heart";
+		 }
+		 public String Clap(){
+			 return "Claps victoriously";
+		 }
+		 public String Pump(){
+			 return "pumps his fist in the air happily";
+		 }
+}
