154cea8fc2ed24b140f85a07b0c6336d4ef15cc5
crystalsciarrino <crystal.sciarrino@gmail.com>
2017-03-19 22:40:04 -0500

    Create Applet.java

---
+++ Milestone assignment/Applet/Applet.java
@@ -0,0 +1,15 @@
+/** I Tried many hours to get my applet to work however I kept recieving this error.
+* I know I will lose a lot of points for this
+* However I do not have an applet to submit. 
+**/
+
+load: PlayerManager.class can't be instantiated.
+java.lang.InstantiationException: PlayerManager
+	at java.lang.Class.newInstance(Unknown Source)
+	at sun.applet.AppletPanel.createApplet(Unknown Source)
+	at sun.applet.AppletPanel.runLoader(Unknown Source)
+	at sun.applet.AppletPanel.run(Unknown Source)
+	at java.lang.Thread.run(Unknown Source)
+Caused by: java.lang.NoSuchMethodException: PlayerManager.<init>()
+	at java.lang.Class.getConstructor0(Unknown Source)
+	... 5 more
